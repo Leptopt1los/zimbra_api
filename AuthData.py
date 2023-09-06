5738d70f982a106c7788a28ecd0935bac55241d9
Methodius <leptoptilos@icloud.com>
2023-09-07 02:31:26 +0900

    AuthData refactored

--- AuthData.py
+++ AuthData.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import config
 from ResponseData import ResponseData
 
 
@@ -14,17 +13,40 @@ class AuthData:
 
     def UpdateAuthData(self) -> ResponseData:
         result = ResponseData()
-        RequestData = f'<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"><soap:Header><context xmlns="urn:zimbra"><userAgent name="ZimbraWebClient - GC107 (Win)"/><session/><authTokenControl voidOnExpired="1"/><format type="js"/></context></soap:Header><soap:Body><AuthRequest xmlns="urn:zimbraAdmin"><name>{self.__Username}</name><password>{self.__Password}</password><virtualHost>mail.amursu.ru</virtualHost><csrfTokenSecured>1</csrfTokenSecured></AuthRequest></soap:Body></soap:Envelope>'
-        AuthResponse = requests.post(self.__AuthURL, data=RequestData, verify=False)
+        RequestData = (
+            '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">'
+                "<soap:Header>"
+                    '<context xmlns="urn:zimbra">'
+                        '<authTokenControl voidOnExpired="1"/>'
+                        '<format type="js"/>'
+                    "</context>"
+                "</soap:Header>"
+                "<soap:Body>"
+                    '<AuthRequest xmlns="urn:zimbraAdmin">'
+                        f"<name>{self.__Username}</name>"
+                        f"<password>{self.__Password}</password>"
+                        "<csrfTokenSecured>1</csrfTokenSecured>"
+                    "</AuthRequest>"
+                "</soap:Body>"
+            "</soap:Envelope>"
+        )
+
         try:
-            self.__AuthToken = AuthResponse.cookies["ZM_ADMIN_AUTH_TOKEN"]
-            self.__CSRFToken = AuthResponse.headers["X-Zimbra-Csrf-Token"]
-        except:
-            x = json.loads(AuthResponse.text)
-            print(x)
-            result.SetErrorText(x["Body"]["Fault"]["Reason"]["Text"])
-            result.SetErrorCode(x["Body"]["Fault"]["Detail"]["Error"]["Code"])
-        result.SetStatusCode(AuthResponse.status_code)
+            AuthResponse = requests.post(self.__AuthURL, data=RequestData, verify=False)
+        except requests.exceptions.RequestException as e:
+            result.SetErrorCode(str(type(e)))
+            result.SetErrorText(str(e))
+            return result
+
+        self.__AuthToken = AuthResponse.cookies.get(["ZM_ADMIN_AUTH_TOKEN"])
+        self.__CSRFToken = AuthResponse.headers.get(["X-Zimbra-Csrf-Token"])
+
+        if None in (self.__AuthToken, self.__CSRFToken):
+            jsonResponseData = json.loads(AuthResponse.text)["Body"]
+
+            result.SetErrorText(jsonResponseData["Fault"]["Reason"]["Text"])
+            result.SetErrorCode(jsonResponseData["Fault"]["Detail"]["Error"]["Code"])
+
         return result
 
     def GetAuthToken(self) -> str:
